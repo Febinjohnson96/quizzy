7f7931a5a25056290bcedc636de0aac11877434e
Febin Johnson <febinnj1996@gmail.com>
2024-09-24 12:47:40 +0530

    added route for home

--- lib/core/config/app_routes.dart
+++ lib/core/config/app_routes.dart
@@ -1,10 +1,15 @@
 import 'package:go_router/go_router.dart';
 import 'package:quizzy/core/config/route_name.dart';
+import 'package:quizzy/presentation/home/home_ui.dart';
 import 'package:quizzy/presentation/splash/splash_ui.dart';
 
 final GoRouter appRoutes = GoRouter(routes: [
   GoRoute(
     path: RouteName.splash,
     builder: (context, state) => const SplashUi(),
-  )
+  ),
+  GoRoute(
+    path: RouteName.home,
+    builder: (context, state) => const HomeUi(),
+  ),
 ]);
