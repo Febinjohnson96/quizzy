9b34ef7fd2b4ee3ae11ae53d13cee1671a4dae0e
Febin Johnson <febinnj1996@gmail.com>
2024-09-26 22:38:24 +0530

    completed login page (#5)

--- lib/core/config/route_name.dart
+++ lib/core/config/route_name.dart
@@ -1,6 +1,7 @@
 class RouteName{
   RouteName._();
   static const String splash = '/';
+  static const String login = '/login';
   static const String home = '/home';
   static const String start = '/start';
   static const String quiz = '/quiz';
